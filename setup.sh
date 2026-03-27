4c1934649f298dbecd2f54bfe227eca36fd39d79
ardwetha <boeckchensemail@gmx.de>
2026-03-27 11:24:33 +0100

    Updates

--- setup.sh
+++ setup.sh
@@ -4,6 +4,6 @@ python3 -m venv .venv
 source .venv/bin/activate
 pip install --upgrade pip
 pip install notebook
-pip install scikit-learn numpy pandas matplotlib seaborn statsmodels
+pip install scikit-learn numpy pandas matplotlib seaborn statsmodels nbconvert
 
 
